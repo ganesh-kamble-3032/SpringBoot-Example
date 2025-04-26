1016833d981a46eaea0251f1dac065cbf46078f1
ganesh-kamble-3032 <ganeshkamble9674@gmail.com>
2025-04-26 12:11:57 +0530

    Update SpringBootFristExampleApplication.java

--- src/main/java/com/car/SpringBootFristExample/SpringBootFristExampleApplication.java
+++ src/main/java/com/car/SpringBootFristExample/SpringBootFristExampleApplication.java
@@ -9,6 +9,7 @@ public class SpringBootFristExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFristExampleApplication.class, args);
 		System.out.println("Spring Boot Starts");
+		System.out.println("Welcome");
 	}
 
 }
